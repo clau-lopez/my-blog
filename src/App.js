885d7ddea794e05aef71a2ac7140f7a08c4a1ebf
Claudia Lopez <colopezfuentes@gmail.com>
2021-01-24 21:57:40 -0300

    ADD: Font-face and Theme js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import "./App.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
+import customTheme from "./styles/theme";
+import customFonts from "./styles/font-face";
 import React from "react";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const theme = extendTheme({
-  styles: {
-    global: (props) => ({
-      body: {
-        fontFamily: "FiraCode",
-      },
-      fonts: {
-        body: "FiraCode",
-        heading: "FiraCode",
-        mono: "FiraCode",
-      },
-    }),
-  },
-});
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={extendTheme(customTheme)}>
+      <Global styles={customFonts} />
       <Router>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
-
         <Switch>
           <Route path="/contact" component={Contact} />
           <Route path="/" component={Home} />
---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+const customTheme = {
+  fonts: {
+    body: "FiraCode-Regular",
+    heading: "FiraCode-Bold",
+  },
+  styles: {
+    global: {
+      "html, body": {
+        fontSize: "sm",
+        lineHeight: "tall",
+      },
+
+      h1: {
+        display: "block",
+        fontSize: "2em",
+        marginBlockStart: "0.67em",
+        marginBlockEnd: "0.67em",
+        marginInlineStart: "0px",
+        marginInlineEnd: "0px",
+        fontWeight: "bold",
+      },
+    },
+  },
+};
+export default customTheme;
